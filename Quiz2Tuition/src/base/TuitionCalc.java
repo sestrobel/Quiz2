2ed7ca3723ac91a0cf72a95acd0ea7b885bf4778
Elizabeth <Elizabeth@valeries-air.fios-router.home>
2016-09-25 11:54:33 -0400

    made tuition an input, not just 12342.

--- Quiz2Tuition/src/base/TuitionCalc.java
+++ Quiz2Tuition/src/base/TuitionCalc.java
@@ -5,15 +5,15 @@ import java.text.DecimalFormat;
 
 public class TuitionCalc {
 	public static void main (String[] args) {
-		double tuition = 12342;
-		double sum = 12342;
 		DecimalFormat money = new DecimalFormat("$0,000.00");
 		Scanner s = new Scanner(System.in);
-		System.out.println("You pay " + money.format(tuition) +" for year 1.");
+		System.out.println("What's the tuition for year 1?");
+		double tuition = s.nextDouble();
+		double sum = tuition;
 		for (int years = 2; years < 5; years++) {
+			System.out.println("You pay " + money.format(tuition) +".");
 			System.out.println("What's the percent increase for year " + years + "?");
 			tuition *= ((s.nextDouble())/100 + 1);
-			System.out.println("You pay " + money.format(tuition) +".");
 			sum += tuition;
 		}
 		System.out.println("You pay " + money.format(sum) + " in total. Congrats!");
